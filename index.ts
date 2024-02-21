ef1b69465b5c95f324027c5c00ebd09b42872f9a
HTselenium <124279315+HTselenium@users.noreply.github.com>
2024-02-21 13:36:20 +0800

    Added new function for division

--- index.ts
+++ index.ts
@@ -13,9 +13,15 @@ class Calculator {
     multiply(a: number, b: number): number {
         return a * b;
     }
+
+    // Method for division
+    division(a: number, b:number) number {
+        return a / b;
+    }
 }
 
 let calculator = new Calculator();
 console.log(`Addition: ${calculator.add(5, 3)}`);
 console.log(`Subtraction: ${calculator.subtract(5, 3)}`);
 console.log(`Multiplication: ${calculator.multiply(5, 3)}`);
+console.log(`Division: ${calculator.division(6, 2)}`);
